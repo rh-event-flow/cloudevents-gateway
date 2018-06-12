711a16b5eabea71bacec70c24c5399890d33019e
Matthias Wessendorf <matzew@apache.org>
2018-06-12 10:36:31 +0200

    :boom: From EventBus to global Kafka event topic

--- src/main/java/io/streamzi/router/verticle/eb/EventFilterVerticle.java
+++ src/main/java/io/streamzi/router/verticle/eb/EventFilterVerticle.java
@@ -3,9 +3,17 @@ package io.streamzi.router.verticle.eb;
 import io.streamzi.cloudevents.CloudEvent;
 import io.streamzi.cloudevents.impl.CloudEventImpl;
 import io.vertx.core.json.Json;
+import io.vertx.kafka.client.producer.KafkaWriteStream;
+import io.vertx.kafka.client.serialization.JsonObjectSerializer;
+import io.vertx.reactivex.config.ConfigRetriever;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.eventbus.EventBus;
+import io.vertx.reactivex.kafka.client.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.util.Map;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class EventFilterVerticle extends AbstractVerticle {
@@ -14,12 +22,26 @@ public class EventFilterVerticle extends AbstractVerticle {
 
     public static final String CE_ADDRESS = "couldEvent";
     private EventBus eventBus;
+    private KafkaWriteStream<String, JsonObjectSerializer> writeStream;
+    private KafkaProducer<String, String> producer;
 
     @Override
     public void start() throws Exception {
         logger.info("Starting EventBus filter/consumer");
+
+        ConfigRetriever retriever = ConfigRetriever.create(vertx);
+
+        retriever.rxGetConfig().subscribe(myconf -> {
+            final Map config = new Properties();
+            config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, myconf.getString("KAFKA_SERVICE_HOST") + ":"  + myconf.getInteger("KAFKA_SERVICE_PORT").toString());
+
+            producer = KafkaProducer.create(vertx, config, String.class, String.class);
+            writeStream = KafkaWriteStream.create(vertx.getDelegate(), config, String.class, String.class);
+        });
+
         assignEventBus();
         registerHandler();
+
     }
 
     private void assignEventBus() {
@@ -31,10 +53,10 @@ public class EventFilterVerticle extends AbstractVerticle {
                 eventBus.consumer(CE_ADDRESS)
                 .toFlowable()
                 .subscribe(message -> {
-                    CloudEvent ce = Json.decodeValue(message.body().toString(), CloudEventImpl.class);
 
+                    CloudEvent ce = Json.decodeValue(message.body().toString(), CloudEventImpl.class);
                     logger.info("Reveived on EB: " + ce.getEventType());
-                    // todo: put to Kafka!
+                    writeStream.write(new ProducerRecord(ce.getEventType(), ce.getEventID(), message.body().toString()));
                 });
     }
 }
