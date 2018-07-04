0a12a266ace55c9816b4db6aff8f10b1a75c5b2c
Matthias Wessendorf <matzew@apache.org>
2018-07-04 16:00:50 +0200

    All modules

---
+++ http-source/src/main/java/io/streamzi/router/source/http/HttpEventSourceVerticle.java
@@ -0,0 +1,57 @@
+package io.streamzi.router.source.http;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.streamzi.router.base.StrombrauBaseVerticle;
+import io.vertx.core.json.Json;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.reactivex.core.http.HttpServer;
+import io.vertx.reactivex.core.http.HttpServerRequest;
+
+import io.streamzi.cloudevents.impl.CloudEventImpl;
+import io.streamzi.cloudevents.CloudEvent;
+
+import java.util.logging.Logger;
+
+public class HttpEventSourceVerticle extends StrombrauBaseVerticle {
+
+    private static final Logger logger = Logger.getLogger(HttpEventSourceVerticle.class.getName());
+
+    @Override
+    public void startStromBrauVerticle(final ConfigRetriever retriever) {
+        logger.info("\uD83C\uDF7A \uD83C\uDF7A Starting HTTP Ingest Verticle");
+
+        final HttpServer server = vertx.createHttpServer();
+        final Flowable<HttpServerRequest> requestFlowable = server.requestStream().toFlowable();
+
+        requestFlowable.subscribe(httpServerRequest -> {
+
+            final Observable<CloudEvent> observable = httpServerRequest
+                    .toObservable()
+                    .compose(io.vertx.reactivex.core.ObservableHelper.unmarshaller(CloudEventImpl.class));
+
+            observable.subscribe(cloudEvent -> {
+
+                if (httpServerRequest.path().equals("/ce")) {
+                    logger.info("Received Event-Type: " + cloudEvent.getEventType());
+
+                    // todo: proper encoding
+                    // ship it!
+                    eventBus.publish("couldEvent", Json.encode(cloudEvent));
+
+
+                } else {
+                    logger.fine("Ignoring request");
+                }
+            });
+
+            // finish the incoming request, with a ACCEPT response...
+            httpServerRequest.response().setChunked(true)
+                    .putHeader("content-type", "text/plain")
+                    .setStatusCode(201) // accepted
+                    .end("Event received");
+        });
+
+        server.listen(8081);
+    }
+}
---
+++ kafka-source/src/main/java/io/streamzi/router/source/kafka/KafkaEventSourceVerticle.java
@@ -0,0 +1,55 @@
+package io.streamzi.router.source.kafka;
+
+import io.reactivex.Flowable;
+import io.streamzi.router.base.StrombrauBaseVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.kafka.client.serialization.BufferDeserializer;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.reactivex.kafka.client.consumer.KafkaConsumer;
+import io.vertx.reactivex.kafka.client.consumer.KafkaConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class KafkaEventSourceVerticle extends StrombrauBaseVerticle {
+
+    public static final String CE_ADDRESS = "couldEvent";
+
+
+    private static final Logger logger = Logger.getLogger(KafkaEventSourceVerticle.class.getName());
+
+    @Override
+    protected void startStromBrauVerticle(final ConfigRetriever retriever) {
+        logger.info("Starting Kafka Ingest Verticle");
+
+
+        retriever.rxGetConfig().subscribe(myconf -> {
+
+            final Map config = new Properties();
+            config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, myconf.getString("MY_CLUSTER_KAFKA_SERVICE_HOST") + ":"  + myconf.getInteger("MY_CLUSTER_KAFKA_SERVICE_PORT").toString());
+            config.put(ConsumerConfig.GROUP_ID_CONFIG, "my_group");
+            config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, BufferDeserializer.class);
+            config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, BufferDeserializer.class);
+
+            Flowable<KafkaConsumerRecord<Buffer, Buffer>> stream = KafkaConsumer.<String, JsonObject>create(vertx, config)
+                    .subscribe("gw.global.input.cloudevents")
+                    .toFlowable();
+
+            stream.subscribe(data -> {
+
+                // pump to EB:
+                logger.info("Received Event-Type on Kafka endpoint");
+                try {
+                    final JsonObject jsonObject = data.value().toJsonObject();
+                    eventBus.publish(CE_ADDRESS, jsonObject);
+                } catch (Exception e) {
+                    logger.warning("Cloud not parse data: " + data.value());
+                }
+            });
+        });
+    }
+}
