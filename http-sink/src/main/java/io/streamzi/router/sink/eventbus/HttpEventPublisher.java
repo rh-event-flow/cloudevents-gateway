c90bb1c7f2de9cbb1c99c9a87d2ad4fa4299c194
Matthias Wessendorf <matzew@apache.org>
2018-07-06 13:09:40 +0200

    KafkaConsumer to HTTP

--- http-sink/src/main/java/io/streamzi/router/sink/eventbus/HttpEventPublisher.java
+++ http-sink/src/main/java/io/streamzi/router/sink/eventbus/HttpEventPublisher.java
@@ -1,13 +1,24 @@
 package io.streamzi.router.sink.eventbus;
 
+import io.reactivex.Flowable;
 import io.streamzi.cloudevents.CloudEvent;
 import io.streamzi.cloudevents.impl.CloudEventImpl;
 import io.streamzi.router.base.StrombrauBaseVerticle;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.kafka.client.producer.KafkaWriteStream;
+import io.vertx.kafka.client.serialization.BufferDeserializer;
 import io.vertx.reactivex.config.ConfigRetriever;
 import io.vertx.reactivex.core.http.HttpClient;
+import io.vertx.reactivex.kafka.client.consumer.KafkaConsumer;
+import io.vertx.reactivex.kafka.client.consumer.KafkaConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.util.Map;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class HttpEventPublisher extends StrombrauBaseVerticle {
@@ -20,25 +31,34 @@ public class HttpEventPublisher extends StrombrauBaseVerticle {
 
     @Override
     public void startStromBrauVerticle(final ConfigRetriever retriever) {
-        logger.info("Starting Event Router");
-
+        logger.info("Kafka to HTTP Bridge");
         client = vertx.createHttpClient();
-
         retriever.rxGetConfig().subscribe(myconf -> registerHandler(myconf));
     }
 
     private void registerHandler(final JsonObject myconf) {
 
-                eventBus.consumer(CE_ADDRESS)
-                .toFlowable()
-                .subscribe(message -> {
 
-                    final CloudEvent ce = Json.decodeValue(message.body().toString(), CloudEventImpl.class);
-                    logger.info("Publishing event to HTTP (" + ce.getEventType() +")");
+        final Map consumerConfig = new Properties();
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, myconf.getString("MY_CLUSTER_KAFKA_SERVICE_HOST") + ":" + myconf.getInteger("MY_CLUSTER_KAFKA_SERVICE_PORT").toString());
+        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "io.streamzi.kafka.cloudevent.type." + myconf.getString("STREAMZI_EVENT_TYPE"));
+        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, BufferDeserializer.class);
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, BufferDeserializer.class);
 
-                    client.post(myconf.getString("HTTP_OUTPUT_ENDPOINT"), response -> {
-                        System.out.println("Received response with status code " + response.statusCode());
-                    }).putHeader("content-type", "text/plain").end(message.body().toString());
-                });
+
+        Flowable<KafkaConsumerRecord<Buffer, Buffer>> stream = KafkaConsumer.<String, JsonObject>create(vertx, consumerConfig)
+                .subscribe("gw.global.input.cloudevents")
+                .toFlowable();
+
+        stream.subscribe(data -> {
+
+            final JsonObject jsonObject = data.value().toJsonObject();
+            logger.info("Publishing event to HTTP: (" + jsonObject.getString("eventType") + ")");
+
+            client.post(myconf.getString("HTTP_OUTPUT_ENDPOINT"), response -> {
+                System.out.println("Received response with status code " + response.statusCode());
+            }).putHeader("content-type", "text/plain").end(jsonObject.toString());
+        });
     }
 }
